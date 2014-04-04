c198a1cc6f9039890f17a0c369ab918c038bc351
M Rosecrance <mrosecrance@yahoo.com>
2014-04-04 13:22:53 -0700

    images

--- SpeechTutor/src/com/example/speechtutor/Playback.java
+++ SpeechTutor/src/com/example/speechtutor/Playback.java
@@ -2,7 +2,9 @@ package com.example.speechtutor;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.MenuItem;
 
 public class Playback extends Activity {
 
@@ -17,6 +19,19 @@ public class Playback extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.playback, menu);
 		return true;
+	}
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+	    // Handle item selection
+	    switch (item.getItemId()) {
+	        case R.id.action_record:
+	        	startActivity(new Intent(this, Record.class));
+	            return true;
+	        case R.id.action_playback:
+	            return true;
+	        default:
+	            return super.onOptionsItemSelected(item);
+	    }
 	}
 
 }
