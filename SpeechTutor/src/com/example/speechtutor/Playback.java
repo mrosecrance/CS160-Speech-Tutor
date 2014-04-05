0c1f89163e4f7b911f6a77150760839f31fd902c
Michelle Nguyen <aimichelle@berkeley.edu>
2014-04-05 12:00:13 -0700

    Added listview for recordings

--- SpeechTutor/src/com/example/speechtutor/Playback.java
+++ SpeechTutor/src/com/example/speechtutor/Playback.java
@@ -1,10 +1,17 @@
 package com.example.speechtutor;
 
-import android.os.Bundle;
+import java.io.File;
+import java.util.ArrayList;
+
 import android.app.Activity;
 import android.content.Intent;
+import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class Playback extends Activity {
 
@@ -12,6 +19,35 @@ public class Playback extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_playback);
+		
+		ListView recordings;
+	    ArrayList<String> FilesInFolder = GetFiles("/sdcard/SpeechTutor");
+	    recordings = (ListView)findViewById(R.id.recordingsList);
+
+	    recordings.setAdapter(new ArrayAdapter<String>(this,
+	        android.R.layout.simple_list_item_1, FilesInFolder));
+
+	   /* lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+	    public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
+	            // Clicking on items
+	         }
+	    });*/
+	}
+	
+	public ArrayList<String> GetFiles(String DirectoryPath) {
+	    ArrayList<String> MyFiles = new ArrayList<String>();
+	    File f = new File(DirectoryPath);
+
+	    f.mkdirs();
+	    File[] files = f.listFiles();
+	    if (files.length == 0)
+	        return null;
+	    else {
+	        for (int i=0; i<files.length; i++) 
+	            MyFiles.add(files[i].getName());
+	    }
+
+	    return MyFiles;
 	}
 
 	@Override
