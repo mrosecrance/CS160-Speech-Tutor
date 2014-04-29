bff97c1f34850d5e53fa4d8d4f7abfb065b1332f
M Rosecrance <mrosecrance@yahoo.com>
2014-04-28 22:54:24 -0700

    Merge branch 'master' of https://github.com/Vadskye/CS160-Speech-Tutor
    Conflicts:
    	SpeechTutor/libs/armeabi-v7a/libpocketsphinx_jni.so
    	SpeechTutor/libs/armeabi/libpocketsphinx_jni.so
    	SpeechTutor/libs/mips/libpocketsphinx_jni.so
    	SpeechTutor/libs/pocketsphinx-android-0.8-nolib.jar
    	SpeechTutor/libs/x86/libpocketsphinx_jni.so

---
+++ SpeechTutor/src/com/example/speechtutor/Statistics.java
@@ -0,0 +1,44 @@
+package com.example.speechtutor;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.view.Menu;
+import android.view.View;
+
+public class Statistics extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_statistics);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.statistics, menu);
+		return true;
+	}
+	public void navigate(View view) {
+		Class classToStart = null;
+		switch(view.getId()) {
+		case R.id.nav_playback: 
+			classToStart = Playback.class;
+			break;
+		case R.id.nav_record:
+			classToStart = Record.class;
+			break;
+		case R.id.nav_statistics:
+			classToStart = Statistics.class;
+			break;
+		case R.id.nav_settings:
+			classToStart = Settings.class;
+			break;
+		}
+		Intent intent = new Intent(this, classToStart);
+		intent.setFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
+		startActivity(intent);
+	}
+
+}
