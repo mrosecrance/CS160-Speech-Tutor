e856131b323316e4978bbd32522e0b18818d93e9
Michelle Nguyen <aimichelle@berkeley.edu>
2014-05-02 19:08:40 -0700

    Removed record/playback from action bar

--- SpeechTutor/src/com/example/speechtutor/Playback.java
+++ SpeechTutor/src/com/example/speechtutor/Playback.java
@@ -84,19 +84,6 @@ public class Playback extends Activity {
 		getMenuInflater().inflate(R.menu.playback, menu);
 		return true;
 	}
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-	    // Handle item selection
-	    switch (item.getItemId()) {
-	        case R.id.action_record:
-	        	startActivity(new Intent(this, Record.class));
-	            return true;
-	        case R.id.action_playback:
-	            return true;
-	        default:
-	            return super.onOptionsItemSelected(item);
-	    }
-	}	
 
 	public void navigate(View view) {
 		Class classToStart = null;
